c18720e2985b7759594ce4289d96536da2813947
learncodealways <danmadan2722@gmail.com>
2023-11-03 18:49:12 +0700

    Menambahkan file function.php

---
+++ phpdasar/function.php
@@ -0,0 +1,26 @@
+<?php
+// function
+ /* function adalah block kode program yang akan berjalan saat kita panggil, untuk membuat function diPHP kita bisa menggunakan kata kunci function, lalu diikuti dengan nama function, kurung()dan diakhiri dengan block{}, untuk memanggil function kita menggunakan nama function lalu diikuti dengan kurung()
+ dibahasa pemrograman lain, function juga disebut dengan method 
+ 
+ syntax function
+ function namafunction (){
+    // statement(pernyataan)
+ }
+// panggil function
+namafunction();
+*/
+
+// contoh penggunaan function
+
+function hiPerson(){
+    echo "Halo saya Rama sedang belajar bahasa pemrograman PHP".PHP_EOL;
+}
+hiPerson();
+hiPerson();
+hiPerson();
+hiPerson();
+hiPerson();
+// hiPerson(); menampilkan output "Halo saya Rama sedang belajar bahasa pemrograman PHP", karena hiPerson(); dipanggil sebanyak 5 kali maka output yang dicetak pun sebanyak 5 kali juga.
+
+
